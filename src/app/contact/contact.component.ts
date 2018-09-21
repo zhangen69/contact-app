8e0065d3e89ec48a862c6fbb656d3f2ca33209ae
Jacob <zhangen69@gmail.com>
2018-09-22 01:31:28 +0800

    added snackbar in dialog when it's closed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { EDialogStatus } from '../enums/EDialogStatus.enum';
 
 @Component({
   selector: 'app-contact',
@@ -7,19 +8,26 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  dialogStatusOptions: string[] = Object.keys(EDialogStatus).splice(Object.keys(EDialogStatus).length / 2);
+  dialogInfo: any = {};
 
-  constructor(public dialogRef: MatDialogRef<ContactComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(public dialogRef: MatDialogRef<ContactComponent>, @Inject(MAT_DIALOG_DATA) public data: any, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
 
-  cancel(): void {
-    this.dialogRef.close();
+  cancel() {
+    const data = Object.assign({}, this.dialogInfo, { status: EDialogStatus.Cancelled, statusName: this.dialogStatusOptions[EDialogStatus.Cancelled] });
+
+    this.dialogRef.close(data);
   }
 
   submit() {
+    const data = Object.assign({}, this.dialogInfo, { status: EDialogStatus.Succeeeded, statusName: this.dialogStatusOptions[EDialogStatus.Succeeeded] });
 
+    this.snackBar.open('Created new contact successfully!', 'Close', { duration: 2000 });
+
+    this.dialogRef.close(data);
   }
 
 }
---
+++ src/app/enums/EDialogStatus.enum.ts
@@ -0,0 +1,4 @@
+export enum EDialogStatus {
+    Cancelled,
+    Succeeeded
+}
